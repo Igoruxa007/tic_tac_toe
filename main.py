d9094cc098c8f425f55a3ea29fe38d9fbb4998ec
Игорь Бородий <igoruxa007@gmail.com>
2023-11-18 23:38:13 +0300

    Create main

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+def clear_window():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+while True:
+    input("Для начала нажмите Enter")
+    clear_window()
+    while True:
+        pass
